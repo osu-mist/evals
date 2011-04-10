bd57f27ae2dbe5fc89693abf4cc25d947a3e8788
Jose Cedeno <jose.cedeno@oregonstate.edu>
2011-04-10 12:06:10 -0700

    re #15455 Add Actions class
        The Actions class acts as a glue between the Portlet class and the
        Hibernate classes. The Portlet processAction and doView call
        methods on the Actions class. The Actions class takes care of
        creating the POJOs and calling the Hibernate classes to take care
        of the business logic.

---
+++ docroot/WEB-INF/src/edu/osu/cws/pass/portlet/Actions.java
@@ -0,0 +1,71 @@
+package edu.osu.cws.pass.portlet;
+
+import edu.osu.cws.pass.models.CriterionArea;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+/**
+ * Actions class used to map user form actions to respective class methods.
+ */
+public class Actions {
+    // criteria, system configuration,
+
+    /**
+     * Takes the request object and creates POJO objects. Then it calls the respective
+     * Hibernate util classes passing the POJOs to handle the saving of data and
+     * validation.
+     *
+     * @param actionRequest
+     * @param actionResponse
+     */
+    public void addCritera(ActionRequest actionRequest, ActionResponse actionResponse) {
+//        @todo: takes the request object and create the POJO objects
+//        @todo: calls the hibernate method createNewCriterion passing on the POJOs
+    }
+
+    /**
+     * Takes the request object, fetches POJO object using hibernate. Sets new fields
+     * using setter methods on POJO. Calls hibernate method to save data back to db.
+     *
+     * @param actionRequest
+     * @param actionResponse
+     */
+    public void editCritera(ActionRequest actionRequest, ActionResponse actionResponse) {
+//        @todo: takes the request object, fetches the
+    }
+
+    /**
+     * Takes the request object and passes the employeeType to the hibernate util class.
+     * It returns an array of CriterionArea POJO.
+     *
+     * @param actionRequest
+     * @param actionResponse
+     */
+    public CriterionArea[] listCriteria(ActionRequest actionRequest, ActionResponse actionResponse) {
+        return new CriterionArea[2];
+    }
+
+    /**
+     * Takes the request object and uses the CriterionAreaID to call the hibernate util
+     * and have it delete the CriterionArea.
+     *
+     * @param actionRequest
+     * @param actionResponse
+     */
+    public void deleteCriteria(ActionRequest actionRequest, ActionResponse actionResponse) {
+
+    }
+
+    /**
+     * This method uses the request object to get a string with the new order. It then calls
+     * a method in the hibernate util class to update the sequence of criterion for the given
+     * employeeType.
+     *
+     * @param actionRequest
+     * @param actionResponse
+     */
+    public void updateCriteriaSequence(ActionRequest actionRequest, ActionResponse actionResponse) {
+
+    }
+}
