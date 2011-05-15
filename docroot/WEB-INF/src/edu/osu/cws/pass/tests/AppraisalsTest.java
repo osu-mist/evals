cf2a18015740fce9a13148c6bd3932a881138db0
Jose Cedeno <jose.cedeno@oregonstate.edu>
2011-05-15 01:50:31 -0700

    re #16444 Add test+logic to updateAppraisal
    The Appraisals.updateAppraisal method takes as a parameter an appraisal
    pojo. It then uses that value and tries update it. It saves the
    associated assessments. IF the goals have been modified, it creates a new
    log for that specific goal. Added tests for this method.
    
    I tried to use a whitelist of fields to only save a subset of fields, but
    that was a no go. Hibernate does not support a whitelist when saving
    objects. Also if some of the values are null, we risk loosing data.

--- docroot/WEB-INF/src/edu/osu/cws/pass/tests/AppraisalsTest.java
+++ docroot/WEB-INF/src/edu/osu/cws/pass/tests/AppraisalsTest.java
@@ -7,10 +7,11 @@ import edu.osu.cws.pass.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.util.List;
+import java.util.Date;
 
 public class AppraisalsTest {
 
@@ -25,6 +26,7 @@ public class AppraisalsTest {
      *
      */
     @BeforeClass
+    @BeforeMethod
     public void setUp() {
         HibernateUtil.setEnvironment(HibernateUtil.TESTING);
         DBUnit dbunit = new DBUnit();
@@ -69,27 +71,168 @@ public class AppraisalsTest {
     }
 
     /**
-     * TestNG Dataprovider, returns an array of fields to update and appraisals.
+     * Method that builds an Appraisal with a bunch of data to save
+     *
      * @return
      */
-//    @DataProvider(name = "job")
-//    public Object[][] loadAppraisalSaveList() throws ModelException {
-//        Session hsession = HibernateUtil.getCurrentSession();
-//        Transaction tx = hsession.beginTransaction();
-//        Job job = (Job) hsession.load(Job.class, 1);
-//        Appraisal appraisal = (Appraisal) hsession.load(Appraisal.class,
-//                appraisals.createAppraisal(job));
-//        tx.commit();
-//        employee = employees.findByOnid("luf");
-//
-//        String[] list1 = {"goalsComments", "goalApproverID", "goalApprovedDate"};
-//
-//        return new Object[][] {
-//                {appraisal, list1}
-//        };
-//    }
+    public Appraisal loadAppraisalSaveList() throws ModelException {
+        Session hsession = HibernateUtil.getCurrentSession();
+        Transaction tx = hsession.beginTransaction();
+        Job job = (Job) hsession.load(Job.class, 1);
+        tx.commit();
+
+        int appraisalID =  appraisals.createAppraisal(job);
+        hsession = HibernateUtil.getCurrentSession();
+        tx = hsession.beginTransaction();
+        Appraisal updatedAppraisal = (Appraisal) hsession.load(Appraisal.class, appraisalID);
+        tx.commit();
+
+        employee = employees.findByOnid("luf");
+        updatedAppraisal.setId(appraisalID);
+
+        updatedAppraisal.setGoalApprover(employee);
+        updatedAppraisal.setGoalApprovedDate(new Date());
+        updatedAppraisal.setGoalComments("goal comments data");
+        updatedAppraisal.setResultSubmitDate(new Date());
+        updatedAppraisal.setEvaluation("evaluation text");
+        updatedAppraisal.setRating(1);
+        updatedAppraisal.setHrApprover(employee);
+        updatedAppraisal.setHrApprovedDate(new Date());
+        updatedAppraisal.setReviewStatusID("review id");
+        updatedAppraisal.setHrComments("hr comments text");
+        updatedAppraisal.setEmployeeComments("employee comments");
+        updatedAppraisal.setEmployeeSignedDate(new Date());
+        updatedAppraisal.setEmailType("submit-goals");
+        updatedAppraisal.setEmailCount(5);
+        updatedAppraisal.setCloseOutDate(new Date());
+        updatedAppraisal.setCloseOutBy(employee);
+        updatedAppraisal.setCloseOutReason("close out reason");
+
+        return updatedAppraisal;
+    }
+
+    /**
+     * This method tests that the updateAppraisal throws an exception when an extra field is being updated that is
+     * not allowed.
+     *
+     * @param modifiedAppraisal
+     */
     @Test(groups = {"unitttest"})
-    public void shouldUpdateFieldsInList(){
+    public void updateAppraisalModelData()
+            throws ModelException {
+        Appraisal modifiedAppraisal = loadAppraisalSaveList();
+        appraisals.updateAppraisal(modifiedAppraisal);
+        assert modifiedAppraisal.getModifiedDate() != null :
+                "Appraisal with just appraisal values failed to save";
+    }
+
+    public Appraisal loadAppraisalAssessments() throws ModelException {
+        Session hsession = HibernateUtil.getCurrentSession();
+        Transaction tx = hsession.beginTransaction();
+        Job job = (Job) hsession.load(Job.class, 1);
+        tx.commit();
+
+        int appraisalID =  appraisals.createAppraisal(job);
+        hsession = HibernateUtil.getCurrentSession();
+        tx = hsession.beginTransaction();
+        Appraisal updatedAppraisal = (Appraisal) hsession.load(Appraisal.class, appraisalID);
+        tx.commit();
+        employee = employees.findByOnid("luf");
+
+        updatedAppraisal.setGoalApprover(employee);
+        updatedAppraisal.setGoalApprovedDate(new Date());
+        updatedAppraisal.setGoalComments("goal comments data");
+        updatedAppraisal.setResultSubmitDate(new Date());
+        updatedAppraisal.setEvaluation("evaluation text");
+        updatedAppraisal.setRating(1);
+
+        for (Assessment assessment : updatedAppraisal.getAssessments()) {
+            assessment.setEmployeeResult("employee results txt");
+            assessment.setSupervisorResult("supervisor results txt");
+        }
+        return updatedAppraisal;
+    }
+
+    /**
+     * Returns an appraisal object with some data and assessment results.
+     *
+     * @return
+     * @throws ModelException
+     */
+    public Appraisal loadAppraisalGoals() throws ModelException {
+        Session hsession = HibernateUtil.getCurrentSession();
+        Transaction tx = hsession.beginTransaction();
+        Job job = (Job) hsession.load(Job.class, 1);
+        tx.commit();
+
+        int appraisalID =  appraisals.createAppraisal(job);
+        hsession = HibernateUtil.getCurrentSession();
+        tx = hsession.beginTransaction();
+        Appraisal updatedAppraisal = (Appraisal) hsession.load(Appraisal.class, appraisalID);
+        tx.commit();
+        employee = employees.findByOnid("luf");
+
+        updatedAppraisal.setGoalApprover(employee);
+        updatedAppraisal.setGoalApprovedDate(new Date());
+        updatedAppraisal.setGoalComments("goal comments data");
+        updatedAppraisal.setResultSubmitDate(new Date());
+        updatedAppraisal.setEvaluation("evaluation text");
+        updatedAppraisal.setRating(1);
+
+        for (Assessment assessment : updatedAppraisal.getAssessments()) {
+            assessment.setEmployeeResult("employee results txt");
+            assessment.setSupervisorResult("supervisor results txt");
+        }
+        return updatedAppraisal;
+    }
+
+    @Test(groups = {"unittest"})
+    public void shouldUpdateAppraisalWithResults()
+            throws ModelException {
+        Appraisal modifiedAppraisal = loadAppraisalGoals();
+        appraisals.updateAppraisal(modifiedAppraisal);
+        for (Assessment assessment : modifiedAppraisal.getAssessments()) {
+            assert assessment.getEmployeeResult() != null :
+                    "Appraisal assessments employee result failed to save";
+            assert assessment.getSupervisorResult() != null :
+                    "Appraisal assessments supervisor result failed to save";
+        }
+    }
+
+    @Test(groups = {"unittest"})
+    /**
+     * Edits an appraisal twice and checks to make sure that the goals are logged.
+     */
+    public void shouldCreateGoalLog()
+            throws ModelException {
+
+        Appraisal modifiedAppraisal = loadAppraisalAssessments();
+
+        // Editing the goals for the first time
+        for (Assessment assessment : modifiedAppraisal.getAssessments()) {
+            assessment.setGoal("first edit of goal");
+        }
+        appraisals.setLoggedInUser(modifiedAppraisal.getJob().getEmployeePidm());
+        appraisals.updateAppraisal(modifiedAppraisal);
+        for (Assessment assessment : modifiedAppraisal.getAssessments()) {
+            assert assessment.getGoal() != null :
+                    "Appraisal assessments goals failed to save";
+            assert assessment.getAssessmentLogs().size() == 1 :
+                    "Appraisal assessment goals should have a new log";
+        }
+
+        // Editing the goals for the second time
+        for (Assessment assessment : modifiedAppraisal.getAssessments()) {
+            assessment.setGoal("second edit of goal");
+        }
+        appraisals.setLoggedInUser(new Employees().findByOnid("luf"));
+        appraisals.updateAppraisal(modifiedAppraisal);
+        for (Assessment assessment : modifiedAppraisal.getAssessments()) {
+            assert assessment.getGoal().equals("second edit of goal") :
+                    "Appraisal assessments goals failed to save";
+            assert assessment.getAssessmentLogs().size() == 2 :
+                    "Appraisal assessment goals should have a new log";
+        }
 
     }
 }
--- docroot/WEB-INF/src/edu/osu/cws/pass/util/Appraisals.java
+++ docroot/WEB-INF/src/edu/osu/cws/pass/util/Appraisals.java
@@ -1,14 +1,18 @@
 package edu.osu.cws.pass.util;
 
-import edu.osu.cws.pass.models.Appraisal;
-import edu.osu.cws.pass.models.Job;
-import edu.osu.cws.pass.models.ModelException;
+import edu.osu.cws.pass.models.*;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import java.util.Date;
+import java.util.List;
 
 public class Appraisals {
+    private Employee loggedInUser;
+
+    private Appraisal appraisal = new Appraisal();
+    private CriterionArea criterionArea = new CriterionArea();
+    private Criteria criteria = new Criteria();
 
     /**
      * This method creates an appraisal for the given job by calling the Hibernate
@@ -19,7 +23,9 @@ public class Appraisals {
      * @throws ModelException
      */
     public int createAppraisal(Job job) throws ModelException {
-        Appraisal appraisal = new Appraisal();
+        CriterionDetail detail;
+        Assessment assessment;
+
         appraisal.setJob(job);
         appraisal.setStatus("goals-due");
         //@todo: how do we define the start and end date ?
@@ -28,12 +34,74 @@ public class Appraisals {
         appraisal.setCreateDate(new Date());
 
         if (appraisal.validate()) {
+            List<CriterionArea> criteriaList = criteria.list(job.getAppointmentType().getId());
             Session session = HibernateUtil.getCurrentSession();
             Transaction tx = session.beginTransaction();
             session.save(appraisal);
+
+            // Create assessment and associate it to appraisal
+            for (CriterionArea criterion : criteriaList) {
+                detail = criterion.getCurrentDetail();
+                assessment = new Assessment();
+                assessment.setCriterionDetail(detail);
+                assessment.setAppraisal(appraisal);
+                assessment.setCreateDate(new Date());
+                session.save(assessment);
+
+            }
             tx.commit();
         }
 
         return appraisal.getId();
+    }
+
+    /**
+     * Updates the appraisal object along with the assessment object. If the goals have been
+     * modified, a new record is inserted in the assessments_logs table.
+     *
+     * @param modifiedAppraisal
+     * @return
+     * @throws ModelException
+     */
+    public boolean updateAppraisal(Appraisal modifiedAppraisal) throws ModelException {
+        String originalGoalText;
+        String newGoalText;
+        AssessmentLog assessmentLog;
+
+        // Validate the data first before we try to save anything
+        modifiedAppraisal.validate();
+        for (Assessment assessment : modifiedAppraisal.getAssessments()) {
+            assessment.validate();
+        }
+
+        // Try to save the data
+        Session session = HibernateUtil.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        modifiedAppraisal.setModifiedDate(new Date());
+        int originalAppraisalID = modifiedAppraisal.getId();
+        session.saveOrUpdate(modifiedAppraisal);
+
+        for (Assessment assessment : modifiedAppraisal.getAssessments()) {
+            assessment.setModifiedDate(new Date());
+            session.saveOrUpdate(assessment);
+
+            // Create new assessment log if necessary
+            originalGoalText = assessment.getLastAssessmentLog().getContent();
+            newGoalText = assessment.getGoal();
+            if (!originalGoalText.equals(newGoalText) && newGoalText != null) {
+                assessmentLog = new AssessmentLog();
+                assessmentLog.setCreateDate(new Date());
+                assessmentLog.setAuthor(loggedInUser);
+                assessmentLog.setContent(newGoalText);
+                assessment.addAssessmentLog(assessmentLog);
+                session.save(assessmentLog);
+            }
+        }
+        tx.commit();
+        return true;
+    }
+
+    public void setLoggedInUser(Employee loggedInUser) {
+        this.loggedInUser = loggedInUser;
     }
 }
