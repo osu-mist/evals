5c697b92041cd27ddcb52e08009a2afb093d2932
Jose Cedeno <jose.cedeno@oregonstate.edu>
2011-06-25 22:36:33 -0700

    re #16898 Add Job's hashCode and equals method
    Hibernate requires hashCode and equals method to be implemented in
    order to use composite-id.

--- docroot/WEB-INF/src/edu/osu/cws/pass/models/Job.java
+++ docroot/WEB-INF/src/edu/osu/cws/pass/models/Job.java
@@ -70,6 +70,80 @@ public class Job extends Pass implements Serializable {
         this.suffix = suffix;
     }
 
+    /**
+     * Implementing equals method needed by Hibernate to use composite-ide
+     *
+     * @param o
+     * @return
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Job)) return false;
+
+        Job job = (Job) o;
+
+        if (id != job.id) return false;
+        if (annualInd != null ? !annualInd.equals(job.annualInd) : job.annualInd != null) return false;
+        if (appointmentType != null ? !appointmentType.equals(job.appointmentType) : job.appointmentType != null)
+            return false;
+        if (beginDate != null ? !beginDate.equals(job.beginDate) : job.beginDate != null) return false;
+        if (businessCenterName != null ? !businessCenterName.equals(job.businessCenterName) : job.businessCenterName != null)
+            return false;
+        if (currentSupervisor != null ? !currentSupervisor.equals(job.currentSupervisor) : job.currentSupervisor != null)
+            return false;
+        if (employee != null ? !employee.equals(job.employee) : job.employee != null) return false;
+        if (endDate != null ? !endDate.equals(job.endDate) : job.endDate != null) return false;
+        if (evalDate != null ? !evalDate.equals(job.evalDate) : job.evalDate != null) return false;
+        if (jobEcls != null ? !jobEcls.equals(job.jobEcls) : job.jobEcls != null) return false;
+        if (jobTitle != null ? !jobTitle.equals(job.jobTitle) : job.jobTitle != null) return false;
+        if (orgCodeDescription != null ? !orgCodeDescription.equals(job.orgCodeDescription) : job.orgCodeDescription != null)
+            return false;
+        if (positionClass != null ? !positionClass.equals(job.positionClass) : job.positionClass != null) return false;
+        if (positionNumber != null ? !positionNumber.equals(job.positionNumber) : job.positionNumber != null)
+            return false;
+        if (salaryGrade != null ? !salaryGrade.equals(job.salaryGrade) : job.salaryGrade != null) return false;
+        if (salaryStep != null ? !salaryStep.equals(job.salaryStep) : job.salaryStep != null) return false;
+        if (status != null ? !status.equals(job.status) : job.status != null) return false;
+        if (suffix != null ? !suffix.equals(job.suffix) : job.suffix != null) return false;
+        if (supervisor != null ? !supervisor.equals(job.supervisor) : job.supervisor != null) return false;
+        if (trialInd != null ? !trialInd.equals(job.trialInd) : job.trialInd != null) return false;
+        if (tsOrgCode != null ? !tsOrgCode.equals(job.tsOrgCode) : job.tsOrgCode != null) return false;
+
+        return true;
+    }
+
+    /**
+     * Implementing hashCode method required by Hibernate to use composite-id.
+     *
+     * @return
+     */
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (employee != null ? employee.hashCode() : 0);
+        result = 31 * result + (supervisor != null ? supervisor.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (jobTitle != null ? jobTitle.hashCode() : 0);
+        result = 31 * result + (positionNumber != null ? positionNumber.hashCode() : 0);
+        result = 31 * result + (suffix != null ? suffix.hashCode() : 0);
+        result = 31 * result + (jobEcls != null ? jobEcls.hashCode() : 0);
+        result = 31 * result + (appointmentType != null ? appointmentType.hashCode() : 0);
+        result = 31 * result + (beginDate != null ? beginDate.hashCode() : 0);
+        result = 31 * result + (endDate != null ? endDate.hashCode() : 0);
+        result = 31 * result + (positionClass != null ? positionClass.hashCode() : 0);
+        result = 31 * result + (tsOrgCode != null ? tsOrgCode.hashCode() : 0);
+        result = 31 * result + (orgCodeDescription != null ? orgCodeDescription.hashCode() : 0);
+        result = 31 * result + (businessCenterName != null ? businessCenterName.hashCode() : 0);
+        result = 31 * result + (salaryGrade != null ? salaryGrade.hashCode() : 0);
+        result = 31 * result + (salaryStep != null ? salaryStep.hashCode() : 0);
+        result = 31 * result + (trialInd != null ? trialInd.hashCode() : 0);
+        result = 31 * result + (annualInd != null ? annualInd.hashCode() : 0);
+        result = 31 * result + (evalDate != null ? evalDate.hashCode() : 0);
+        result = 31 * result + (currentSupervisor != null ? currentSupervisor.hashCode() : 0);
+        return result;
+    }
+
     public int getId() {
         return id;
     }
