2c63f54ba194e09e69514fd3d6b1d444599def56
Jose Cedeno <jose.cedeno@oregonstate.edu>
2011-08-09 11:49:15 -0700

    re #16945 Update appraisal creation user testing list
    Updating the list of position numbers to create appraisals during
    the user testing.

--- docroot/WEB-INF/src/edu/osu/cws/pass/tests/AppraisalCreation.java
+++ docroot/WEB-INF/src/edu/osu/cws/pass/tests/AppraisalCreation.java
@@ -21,7 +21,8 @@ public class AppraisalCreation {
         Configuration goalsDueConfig = (Configuration) session.load(Configuration.class, 1);
         String whereClause = "WHERE status != 'T' and appointmentType ='Classified' " +
                 "AND positionNumber in " +
-                "('C30132', 'C30134', 'C30135', 'C30136', 'C30137', 'C30138', 'C30141', 'C30143', 'C30144', 'C30146')";
+//                "('C30132', 'C30134', 'C30135', 'C30136', 'C30137', 'C30138', 'C30141', 'C30143', 'C30144', 'C30146')";
+                "('C13716', 'C14380', 'C30013', 'C30022', 'C12403', 'C14075', 'C30132', 'C30134', 'C30135', 'C30136', 'C16100', 'C30137', 'C10099', 'C10471', 'C30138', 'C30141', 'C18024', 'C30143', 'C10210', 'C30144', 'C25003', 'C30146', 'C30008', 'C30479')";
 
         String query = "from edu.osu.cws.pass.models.Job " + whereClause;
         List<Job> results = (List<Job>) session.createQuery(query).list();
