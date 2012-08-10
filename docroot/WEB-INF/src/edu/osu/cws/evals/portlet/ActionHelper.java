860e331614f5a0cf146c254ae2edcfeaa6a544b4
martin <wanghuay@onid.orst.edu>
2012-08-10 10:22:28 -0700

    re #19451, #20165, #20787, #20809
    * slice the ActionHelper.getLoggedOnUser into two parts, rewrite the
    HomeAction.displayMyInformation and load the jobs for the SQL
    statement in EmployeeMgr.findJobs.
    * In JobMgr we remove the Sql stament that restrict the
    status of the Job in "A"
    * Save a Date attribute loadDate on the portlet context,
     every time the method EvalsPortlet.portletSetup is called,
     we compare this date to the current date, and if the
     difference is more than 7 days, we reload the data
     and reset the contextReloadDate.
    * In ActionHelp, we now save the requestMap in Portletsession
    * The last version of the code in martin-develop will display
     a blanket page of Evals portlet. I cannot find what problem is
     so I copy the code from the branch martin-fixes and do other
     modify base on Jose's review comment.

--- docroot/WEB-INF/src/edu/osu/cws/evals/portlet/ActionHelper.java
+++ docroot/WEB-INF/src/edu/osu/cws/evals/portlet/ActionHelper.java
@@ -726,8 +726,8 @@ public class ActionHelper {
      */
     public void setRequestAttributes(RenderRequest request) {
         PortletSession session = request.getPortletSession(true);
-        String currentRole = getCurrentRole(request);
         HashMap<String,Object> requestMap = (HashMap)session.getAttribute(REQUEST_MAP);
+        String currentRole = getCurrentRole(request);
         requestMap.put("currentRole", currentRole);
 
         for (Map.Entry<String, Object> entry : requestMap.entrySet()) {
@@ -736,11 +736,18 @@ public class ActionHelper {
         requestMap.clear();
     }
 
+    /**
+     * Add the data into requestMap and store the map into session.
+     *
+     * @param key
+     * @param object
+     * @param request
+     * @return
+     */
     public void addToRequestMap(String key, Object object, PortletRequest request) {
         PortletSession session = request.getPortletSession(true);
         HashMap<String,Object> requestMap = (HashMap)session.getAttribute(REQUEST_MAP);
-        if  (requestMap == null)
-        {
+        if  (requestMap == null) {
             requestMap = new HashMap<String, Object>();
             requestMap.put(key, object);
             session.setAttribute(REQUEST_MAP,requestMap);
@@ -748,17 +755,32 @@ public class ActionHelper {
         else {
             requestMap.put(key, object);
         }
+        session.setAttribute(REQUEST_MAP, requestMap);
 
     }
 
+    /**
+     * remove the requestMap in session
+     *
+     * @param request
+     * @param key
+     * @return object from requestMap searching from key
+     */
     public Object getFromRequestMap(String key,PortletRequest request) {
         PortletSession session = request.getPortletSession(true);
         HashMap<String,Object> requestMap = (HashMap)session.getAttribute(REQUEST_MAP);
-        if (requestMap == null)
+        if (requestMap == null){
             return null;
+        }
         return requestMap.get(key);
     }
 
+    /**
+     * remove the requestMap in session
+     *
+     * @param request
+     * @return
+     */
     public void removeRequestMap(PortletRequest request){
         PortletSession session = request.getPortletSession(true);
         session.removeAttribute(REQUEST_MAP);
