cfd932af3dbdf1b14d52264e4b2c9522e11e5857
Jose Cedeno <jose.cedeno@oregonstate.edu>
2013-01-16 14:36:23 -0800

    Add missing Notice model
    re #18696

--- docroot/WEB-INF/src/edu/osu/cws/evals/models/Notice.java
+++ docroot/WEB-INF/src/edu/osu/cws/evals/models/Notice.java
@@ -1,5 +1,7 @@
 package edu.osu.cws.evals.models;
 
+import java.util.Date;
+
 /**
  * Created with IntelliJ IDEA.
  * User: wanghuay
@@ -8,4 +10,65 @@ package edu.osu.cws.evals.models;
  * To change this template use File | Settings | File Templates.
  */
 public class Notice {
+    private int id;
+
+    private int ancestorID;
+
+    private String name;
+
+    private String text;
+
+    private Employee creator;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Employee getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Employee creator) {
+        this.creator = creator;
+    }
+
+    public int getAncestorID() {
+        return ancestorID;
+    }
+
+    public void setAncestorID(int ancestorID) {
+        this.ancestorID = ancestorID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private Date createDate;
+
+
 }
