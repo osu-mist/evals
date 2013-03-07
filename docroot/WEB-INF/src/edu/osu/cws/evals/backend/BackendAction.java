47ee12ee8741ba6a658f715eb045aa08b6213d3a
Jose Cedeno <jose.cedeno@oregonstate.edu>
2013-03-07 14:04:43 -0800

    Flush out the google guice code for the backend
    re #22923
    
    * Adding a few comments to explain the Module and Action class.
    * Call the process method from the BackendAction class
    * Removing constructor and extra code from BackendMgr setup
    * Remove hard coded values from BackendModule

--- docroot/WEB-INF/src/edu/osu/cws/evals/backend/BackendAction.java
+++ docroot/WEB-INF/src/edu/osu/cws/evals/backend/BackendAction.java
@@ -5,11 +5,17 @@ import com.google.inject.Injector;
 
 public class BackendAction {
 
-    public static void main(String[] args) throws Exception
-    {
+    /**
+     * This class is the Driver for the Backend cron job. It takes care of getting an
+     * instance of the BackendMgr and calling the process method to execute things.
+     *
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
         Injector injector = Guice.createInjector(new BackendModule());
         BackendMgr mgr = injector.getInstance(BackendMgr.class);
-        System.out.println(mgr.toString());
+        mgr.process();
     }
 
 }
