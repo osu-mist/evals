96e416b768a5adfb540b95c0cab3502710c2bad2
Jose Cedeno <jose.cedeno@oregonstate.edu>
2013-05-13 16:55:26 -0700

    Fix EmailMgr tests.
    re #23792

--- docroot/WEB-INF/src/edu/osu/cws/evals/tests/EmailsTest.java
+++ docroot/WEB-INF/src/edu/osu/cws/evals/tests/EmailsTest.java
@@ -2,6 +2,9 @@ package edu.osu.cws.evals.tests;
 
 import edu.osu.cws.evals.hibernate.EmailMgr;
 import edu.osu.cws.evals.models.Email;
+import edu.osu.cws.evals.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -28,11 +31,32 @@ public class EmailsTest {
         //@todo: test add
 
         Email email = new Email();
-        email.setAppraisalId(1);
+        email.setAppraisalId(99);
         email.setEmailType("goals-submitted");
         email.setSentDate(new Date());
 
+        Session session = HibernateUtil.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+
+        // Verify that record doesn't exist yet.
+        String verifyQuery = "from edu.osu.cws.evals.models.Email where appraisalId = ? and " +
+                "emailType = ?";
+        boolean recordExists = session.createQuery(verifyQuery)
+                .setInteger(0, 99)
+                .setString(1, "goals-submitted")
+                .list().size() == 1;
+        assert !recordExists : "Email record shouldn't exist";
+
         EmailMgr.add(email);
+
+        // Verify that record was saved
+        recordExists = session.createQuery(verifyQuery)
+                .setInteger(0, 99)
+                .setString(1, "goals-submitted")
+                .list().size() == 1;
+        assert recordExists : "Email record wasn't saved";
+
+        tx.commit();
     }
 
     public void shouldAddAnArrayOfEmails()  throws Exception{
@@ -44,30 +68,56 @@ public class EmailsTest {
         email.setSentDate(new Date());
         emails.add(email);
 
+        email = new Email();
         email.setAppraisalId(2);
         email.setEmailType("goals-required-modification");
+        email.setSentDate(new Date());
         emails.add(email);
 
+        Session session = HibernateUtil.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+
+        String verifyQuery = "from edu.osu.cws.evals.models.Email";
+        Integer initialCount = session.createQuery(verifyQuery).list().size();
+
         EmailMgr.add(emails);
+        session.flush();
+
+        Integer afterAddCount = session.createQuery(verifyQuery).list().size();
+        assert (afterAddCount - initialCount) == emails.size() : "emails were not added";
+
+        tx.commit();
     }
 
     public void shouldReturnFirstEmail() throws Exception {
+        Session session = HibernateUtil.getCurrentSession();
+        Transaction tx = session.beginTransaction();
         Email email = EmailMgr.getFirstEmail(1, "goals-submitted");
+        tx.commit();
         assert email.getId() == 1 : "The first sent email should have been returned";
     }
 
     public void shouldReturnNullIfThereIsNoFirstEmail() throws Exception {
+        Session session = HibernateUtil.getCurrentSession();
+        Transaction tx = session.beginTransaction();
         Email email = EmailMgr.getFirstEmail(1, "goals-due");
+        tx.commit();
         assert email == null : "The return value should be null because there is no row in the db";
     }
 
     public void shouldReturnLastEmail() throws Exception {
+        Session session = HibernateUtil.getCurrentSession();
+        Transaction tx = session.beginTransaction();
         Email email = EmailMgr.getLastEmail(1, "goals-submitted");
+        tx.commit();
         assert email.getId() == 3 : "The first sent email should have been returned";
     }
 
     public void shouldReturnNullIfThereIsNoLastEmail() throws Exception {
+        Session session = HibernateUtil.getCurrentSession();
+        Transaction tx = session.beginTransaction();
         Email email = EmailMgr.getLastEmail(1, "goals-due");
+        tx.commit();
         assert email == null : "The return value should be null because there is no row in the db";
 
     }
