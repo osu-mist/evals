071ffacde21bdf2acfe0b38bc1009baf8f06d291
Jose Cedeno <jose.cedeno@oregonstate.edu>
2013-05-13 16:11:56 -0700

    Fix AppraisalSteps tests
    re #23792

--- docroot/WEB-INF/src/edu/osu/cws/evals/tests/AppraisalStepsTest.java
+++ docroot/WEB-INF/src/edu/osu/cws/evals/tests/AppraisalStepsTest.java
@@ -1,13 +1,16 @@
 package edu.osu.cws.evals.tests;
 
 import edu.osu.cws.evals.hibernate.AppraisalStepMgr;
+import edu.osu.cws.evals.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 
 public class AppraisalStepsTest {
-    AppraisalStepMgr appraisalStepMgr = new AppraisalStepMgr();
 
     /**
      * This setup method is run before this class gets executed in order to
@@ -23,13 +26,15 @@ public class AppraisalStepsTest {
 
     @Test(groups = {"unittest"})
     public void shouldListAllSteps() throws Exception {
-        HashMap steps = appraisalStepMgr.list();
+        Session session = HibernateUtil.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        HashMap steps = AppraisalStepMgr.list();
+        tx.commit();
         //@todo: does the test below make sense without the original status in the appraisal_step
         // table?
         assert steps.containsKey("submit-Classified") : "Missing step in hashmap";
         assert steps.containsKey("require-modification-Classified") : "Missing step in hashmap";
         assert steps.size() == 2 :
                 "AppraisalStepMgr.list() should find all appraisalSteps";
-
     }
 }
