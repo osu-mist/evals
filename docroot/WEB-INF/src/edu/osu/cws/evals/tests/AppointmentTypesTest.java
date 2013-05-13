d39f8813055a81e0480b3e2aee5f30814a58d797
Jose Cedeno <jose.cedeno@oregonstate.edu>
2013-05-13 15:14:01 -0700

    Fix appointment types tests
    re #23792

--- docroot/WEB-INF/src/edu/osu/cws/evals/tests/AppointmentTypesTest.java
+++ docroot/WEB-INF/src/edu/osu/cws/evals/tests/AppointmentTypesTest.java
@@ -2,6 +2,9 @@ package edu.osu.cws.evals.tests;
 
 import edu.osu.cws.evals.hibernate.AppointmentTypeMgr;
 import edu.osu.cws.evals.models.AppointmentType;
+import edu.osu.cws.evals.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -10,20 +13,20 @@ import java.util.List;
 @Test
 public class AppointmentTypesTest {
 
-    AppointmentTypeMgr appointmentTypeMgr = new AppointmentTypeMgr();
-
     @BeforeMethod
     public void initializeObjects() throws Exception {
-        appointmentTypeMgr = new AppointmentTypeMgr();
         DBUnit dbunit = new DBUnit();
         dbunit.seedDatabase();
     }
 
     @Test(groups = {"unittest"})
     public void testList() throws Exception {
-        List types = appointmentTypeMgr.list();
+        Session session = HibernateUtil.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        List types = AppointmentTypeMgr.list();
         assert types.size() == 1 : "Invalid number of appointment types.";
         AppointmentType type = (AppointmentType) types.get(0);
         assert type.getName().equals("Classified") : "Invalid appointment type";
+        tx.commit();
     }
 }
