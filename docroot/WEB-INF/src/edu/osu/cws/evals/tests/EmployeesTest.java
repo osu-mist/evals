cbc3ba6597ee1dc3049156ed7dba46ebe1bd956e
Jose Cedeno <jose.cedeno@oregonstate.edu>
2013-05-13 19:26:34 -0700

    Fix employees test
    re #23792

--- docroot/WEB-INF/src/edu/osu/cws/evals/tests/EmployeesTest.java
+++ docroot/WEB-INF/src/edu/osu/cws/evals/tests/EmployeesTest.java
@@ -14,7 +14,6 @@ import java.util.List;
 @Test
 public class EmployeesTest {
 
-    EmployeeMgr employeeMgr = new EmployeeMgr();
     Transaction tx;
 
     @BeforeMethod
@@ -36,46 +35,28 @@ public class EmployeesTest {
      */
     @Test(groups = {"unittest"})
     public void shouldFindAUserByUsername() throws Exception {
-        Employee employee = employeeMgr.findByOnid("cedenoj", null);
+        Employee employee = EmployeeMgr.findByOnid("cedenoj", null);
 
         assert employee != null : "The employee object should not be null";
         assert employee.getId() == 12345 : "Id should be valid";
         assert employee.getStatus().equals("A") : "Employee should be active";
-
-
     }
 
-    /**
-     * Tests that the employees view is not empty
-     * Before you run this test method make sure that the beforeMehtod in this class is commented out.
-     */
-    public void shouldHaveEmployeesInView() throws Exception {
-        List<Employee> results = EmployeeMgr.list();
-        Employee employee;
-
-        // place a breakpoint below if you want to step through the records to make sure
-        // we are getting data from the view
-        for (int i = 0; i < 5; i++) {
-            employee = results.get(i);
-            assert employee != null;
-        }
-        assert results.size() > 0 : "The list of employees should not be empty";
-    }
-
+    @Test
     public void shouldOnlyAccept9DigitsForOsuid() {
         Employee employee = new Employee();
 
         employee.setOsuid("12345");
-        assert employee.validateOsuid() == false;
+        assert !employee.validateOsuid();
 
         employee.setOsuid(" ");
-        assert employee.validateOsuid() == false;
+        assert !employee.validateOsuid();
 
         employee.setOsuid("123-55-1234");
-        assert employee.validateOsuid() == false;
+        assert !employee.validateOsuid();
 
         employee.setOsuid("123456789");
-        assert employee.validateOsuid() == true;
+        assert employee.validateOsuid();
 
     }
 }
