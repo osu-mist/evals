6cebc10d6c55eb5fa2e6b69a5cb60bb6040eed38
Jose Cedeno <jose.cedeno@oregonstate.edu>
2013-12-12 14:22:21 -0800

    Increase salary recommendation to two decimals
    re #26646
    
    The salary configuration table as speicified by the contract allows
    for two decimals in the percentage increase. The java code and js
    functionality allow for more decimals, but the db was only storing
    1 decimal.

--- sql-updates/2.0.1.sql
+++ sql-updates/2.0.1.sql
@@ -5,3 +5,10 @@ ALTER TABLE salaries ADD (
     one_min number(3,1)
 );
 
+-- re #26646
+ALTER TABLE salaries MODIFY (
+    salary_increase number(4,2),
+    two_increase number(4,2),
+    one_max number(4,2),
+    one_min number(4,2)
+);
