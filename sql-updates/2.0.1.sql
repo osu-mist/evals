b21a3ba70b64ef98cf662318b956a30e5406795c
Jose Cedeno <jose.cedeno@oregonstate.edu>
2013-12-12 14:15:34 -0800

    Update sql datatype in salaries column
    re #26647
    
    The sql update file didn't have the correct datatype for Oracle.
    The datatype was correct in the Oracle salaries db table.

--- sql-updates/2.0.1.sql
+++ sql-updates/2.0.1.sql
@@ -1,7 +1,7 @@
 -- re #26029
 ALTER TABLE salaries ADD (
-    two_increase decimal(3,1), 
-    one_max decimal(3,1), 
-    one_min decimal(3,1)
+    two_increase number(3,1), 
+    one_max number(3,1), 
+    one_min number(3,1)
 );
 
