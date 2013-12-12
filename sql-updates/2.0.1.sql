6e3d2176ca6baf80adf05d2054914ce4dde4c6b7
Jose Cedeno <jose.cedeno@oregonstate.edu>
2013-12-12 14:26:04 -0800

    Update salaries validation values for some evals
    re #26647
    
    Some of the IT evaluations were originally created by the BackendMgr
    and set to goalsDue even though they were past the appraisalDue
    status. These evaluations didn't have the salary records updated by
    the backend, so we had to manually update them so that thew new
    columns would have valid values.

--- sql-updates/2.0.1.sql
+++ sql-updates/2.0.1.sql
@@ -12,3 +12,12 @@ ALTER TABLE salaries MODIFY (
     one_max number(4,2),
     one_min number(4,2)
 );
+
+-- re #26647
+-- the two statements below update the salaries for records
+-- were the Backend mgr didn't update the values.
+UPDATE salaries SET two_increase = 4.75, one_max = 7.25, one_min = 6
+WHERE salary_current < salary_midpoint and one_min is null;
+
+UPDATE salaries SET two_increase = 2, one_max = 4, one_min = 3
+WHERE salary_current >= salary_midpoint and one_min is null;
