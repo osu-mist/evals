05cbbb6ee9ea8297c7300cec1717c6c28952d2de
Colin Bradford <colin.bradford@oregonstate.edu>
2014-01-31 12:34:39 -0800

    Changed value for daysBeforeArchive to 365
    re #26835

--- sql-updates/2.0.2.sql
+++ sql-updates/2.0.2.sql
@@ -1,7 +1,7 @@
 -- re #23787
 -- add autosave configuration value
 insert into configurations values(pass_seq.nextval, 'configuration', 'autoSaveFrequency', 14, 60, null, null);
-insert into configurations values(pass_seq.nextval, 'configuration', 'daysBeforeArchive', 15, 6, null, null);
+insert into configurations values(pass_seq.nextval, 'configuration', 'daysBeforeArchive', 15, 365, null, null);
 insert into status values('archivedCompleted');
 insert into status values('archivedClosed');
 delete from status where status = 'archived';
