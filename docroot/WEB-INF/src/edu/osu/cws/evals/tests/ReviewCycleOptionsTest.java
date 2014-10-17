63d11e28b227386aad949bca173dd91066f85dd9
floppyears <cedenoj@onid.oregonstate.edu>
2014-10-17 13:53:54 -0700

    More review cycle options tests
    EV-538

--- docroot/WEB-INF/src/edu/osu/cws/evals/tests/ReviewCycleOptionsTest.java
+++ docroot/WEB-INF/src/edu/osu/cws/evals/tests/ReviewCycleOptionsTest.java
@@ -41,8 +41,10 @@ public class ReviewCycleOptionsTest {
         List<ReviewCycleOption> optionList = ReviewCycleOptionMgr.list();
         tx.commit();
 
+        int previousSequence = 0;
         for (ReviewCycleOption option : optionList) {
-            assert option.getDeleteDate() == null : "Reason should not be deleted";
+            assert previousSequence <= option.getSequence() : "Sequence should be in ascending order";
+            previousSequence = option.getSequence();
         }
     }
 
@@ -56,6 +58,63 @@ public class ReviewCycleOptionsTest {
 
         assert option.getDeleteDate() != null : "The delete date should be set";
         assert option.getDeleter().getId() == deleterPidm : "The deleter pidm should be set";
+    }
+
+    public void shouldEditOptionWithFormValues() throws Exception {
+        int id = 1;
+        int pidm = 12345;
+        int sequence = 88;
+        int value = 8;
+        String name = "Updated Category";
+
+        Session session = HibernateUtil.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        ReviewCycleOptionMgr.add(name, value, sequence, new Employee(pidm), id);
+        ReviewCycleOption option = ReviewCycleOptionMgr.get(name);
+        tx.commit();
+
+        assert option.getCreator().getId() == pidm;
+        assert option.getSequence() == sequence;
+        assert option.getValue() == value;
+        assert option.getId() == id;
+    }
+
+    public void shouldAddOptionWithFormValues() throws Exception {
+        int pidm = 12345;
+        int sequence = 88;
+        int value = 8;
+        String name = "Testing Add";
+
+        Session session = HibernateUtil.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        ReviewCycleOptionMgr.add(name, value, sequence, new Employee(pidm), null);
+        ReviewCycleOption option = ReviewCycleOptionMgr.get(name);
+        tx.commit();
+
+        assert option.getCreator().getId() == pidm;
+        assert option.getSequence() == sequence;
+        assert option.getValue() == value;
+        assert option.getId() != null;
+    }
+
+    public void shouldUndoDeleteWhenAddingExistingOption() throws Exception {
+        int pidm = 12345;
+        int sequence = 88;
+        int value = 8;
+        String name = "Deleted option";
+
+        Session session = HibernateUtil.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        ReviewCycleOptionMgr.add(name, value, sequence, new Employee(pidm), null);
+        ReviewCycleOption option = ReviewCycleOptionMgr.get(name);
+        tx.commit();
+
+        assert option.getCreator().getId() == pidm;
+        assert option.getSequence() == sequence;
+        assert option.getValue() == value;
+        assert option.getId() == 3 : "It should use id of previous deleted option";
+        assert option.getDeleteDate() == null : "It should clear out deleted values";
+        assert option.getDeleter() == null : "It should clear out deleted values";
     }
 
 }
