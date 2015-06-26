cf94855e0c920156445bcb6a5e297178ddc6bc13
floppyears <cedenoj@onid.oregonstate.edu>
2015-06-26 11:03:02 -0700

    Add configurations and appointment types for rank
    EV-602
    
    The new appointment type needs to be added as well as the same
    configuration options as the ones used for professional faculty.
    The configs for both Ranked and Professional Faculty are the same.

--- sql-updates/3.1.2.sql
+++ sql-updates/3.1.2.sql
@@ -2,3 +2,22 @@
 insert into appraisal_steps as
 select pass_seq.nextval, action, 'Ranked Faculty', new_status, email_type 
 from appraisal_steps where appointment_type = 'Professional Faculty';
+
+-- EV-604
+insert into appointment_types values('Ranked Faculty');
+
+INSERT INTO configurations
+select pass_seq.nextval, section, name, (select max(sequence) from configurations where section = c.section) + rownum as seq, value, reference_point, action, 'Ranked Faculty'
+from configurations c
+where appointment_type != 'Default' and section = 'email-notification';
+
+INSERT INTO configurations
+select pass_seq.nextval, section, name, (select max(sequence) from configurations where section = c.section) + rownum as seq, value, reference_point, action, 'Ranked Faculty'
+from configurations c
+where appointment_type != 'Default' and section = 'configuration';
+
+
+INSERT INTO configurations
+select pass_seq.nextval, section, name, (select max(sequence) from configurations where section = c.section) + rownum as seq, value, reference_point, action, 'Ranked Faculty'
+from configurations c
+where appointment_type != 'Default' and section = 'due-date';
