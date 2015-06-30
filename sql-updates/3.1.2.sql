d46c3079fd3c1a953bbf10d683fa6226da503feb
Taylor Brown <browtayl@onid.oregonstate.edu>
2015-06-30 15:02:09 -0700

    downcase sql and delete whitespace for consistency

--- sql-updates/3.1.2.sql
+++ sql-updates/3.1.2.sql
@@ -1,23 +1,22 @@
 -- EV-602 add appraisal steps for Ranked Faculty
 insert into appraisal_steps as
-select pass_seq.nextval, action, 'Ranked Faculty', new_status, email_type 
+select pass_seq.nextval, action, 'Ranked Faculty', new_status, email_type
 from appraisal_steps where appointment_type = 'Professional Faculty';
 
 -- EV-604
 insert into appointment_types values('Ranked Faculty');
 
-INSERT INTO configurations
+insert into configurations
 select pass_seq.nextval, section, name, (select max(sequence) from configurations where section = c.section) + rownum as seq, value, reference_point, action, 'Ranked Faculty'
 from configurations c
 where appointment_type != 'Default' and section = 'email-notification';
 
-INSERT INTO configurations
+insert into configurations
 select pass_seq.nextval, section, name, (select max(sequence) from configurations where section = c.section) + rownum as seq, value, reference_point, action, 'Ranked Faculty'
 from configurations c
 where appointment_type != 'Default' and section = 'configuration';
 
-
-INSERT INTO configurations
+insert into configurations
 select pass_seq.nextval, section, name, (select max(sequence) from configurations where section = c.section) + rownum as seq, value, reference_point, action, 'Ranked Faculty'
 from configurations c
 where appointment_type != 'Default' and section = 'due-date';
