a31fe7a79efe326db7e8d9741743083b6da29c95
Alex Ruef <alex.ruef@oregonstate.edu>
2019-12-31 14:38:14 -0800

    added missing parameter to submit function

--- docroot/jsp/home/testTools.js
+++ docroot/jsp/home/testTools.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function() {
   console.log('eyo');
 
-  jQuery("#<portlet:namespace />fm.appraisalDue, #<portlet:namespace />fm.appraisalOverdue, #<portlet:namespace />fm.releaseDue, #<portlet:namespace />fm.releaseOverdue").submit(function(){
+  jQuery("#<portlet:namespace />fm.appraisalDue, #<portlet:namespace />fm.appraisalOverdue, #<portlet:namespace />fm.releaseDue, #<portlet:namespace />fm.releaseOverdue").submit(function(event){
     console.log('submit');
   });
 });
