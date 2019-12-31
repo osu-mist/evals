a34b04b8a59dd9acfd89e958be1f36461623229b
Alex Ruef <alex.ruef@oregonstate.edu>
2019-12-31 15:19:32 -0800

    added try catch

--- docroot/jsp/home/testTools.js
+++ docroot/jsp/home/testTools.js
@@ -1,7 +1,11 @@
 jQuery(document).ready(function() {
   console.log('eyo');
 
+  try{
   jQuery("#submitTest").submit(function(event) {
     console.log('submit');
   });
+  } catch (err) {
+    console.log(err);
+  }
 });
