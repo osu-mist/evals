1e89d776da7244e88397ef2b48429658e44de512
Alex Ruef <alex.ruef@oregonstate.edu>
2020-01-03 15:35:24 -0800

    implemented abstract methods

--- docroot/WEB-INF/src/edu/osu/cws/evals/portlet/TestsAction.java
+++ docroot/WEB-INF/src/edu/osu/cws/evals/portlet/TestsAction.java
@@ -2,10 +2,14 @@ package edu.osu.cws.evals.portlet;
 
 import javax.portlet.*;
 
-public class TestsAction {
+public class TestsAction implements ActionInterface {
 
     public String updateTest(PortletRequest request, PortletResponse response) {
       System.out.println("new update");
       return "true";
     }
+
+    public void setErrorHandler(ErrorHandler errorHandler) { this.errorHandler = errorHandler; }
+    public void setHomeAction(HomeAction homeAction) { this.homeAction = homeAction; }
+    public void setActionHelper(ActionHelper actionHelper) { this.actionHelper = actionHelper; }
 }
