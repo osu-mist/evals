32e57a91dc3b15def2044330d2d727f1a0d47b42
Alex Ruef <alex.ruef@oregonstate.edu>
2020-01-06 14:32:00 -0800

    added success message for appraisal creation

--- docroot/jsp/home/testTools.js
+++ docroot/jsp/home/testTools.js
@@ -12,9 +12,18 @@ jQuery(document).ready(function() {
       success: function(msg) {
         if (msg == "success") {
           console.log("we have success");
+
+          jQuery('.portlet-msg-success').remove();
+
+          jQuery("#<portlet:namespace />flash").html(
+            '<span class="portlet-msg-success"><liferay-ui:message key="Appraisal Created"/></span>'
+          );
         } else {
           console.log(msg);
         }
+
+        jQuery('.portlet-msg-success').fadeIn('slow');
+        setTimeout(function() {jQuery('.portlet-msg-success').fadeOut('slow')}, 30000);
       }
     });
   });
