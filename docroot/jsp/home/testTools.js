d0fd204f576194de62c0a2effa0132ae94840859
Alex Ruef <alex.ruef@oregonstate.edu>
2020-01-08 09:31:05 -0800

    fixed include

--- docroot/jsp/home/testTools.js
+++ docroot/jsp/home/testTools.js
@@ -23,7 +23,7 @@ jQuery(document).ready(function() {
         var jspFile = '<%@ include file="' + msg + '"%>';
         console.log(jspFile);
         jQuery("#<portlet:namespace />flash").html(
-          jspFile
+          '<%@ include file="/jsp/home/myInfo.jsp"%>'
         );
         // '<span class="portlet-msg-success"><liferay-ui:message key="Appraisal Created"/></span>'
 
