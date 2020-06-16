86c804f7de5f41a312d46b175ea4e32048757845
Alex Ruef <alex.ruef@oregonstate.edu>
2020-06-16 11:46:09 -0700

    Test with data

--- docroot/jsp/home/testTools.js
+++ docroot/jsp/home/testTools.js
@@ -9,7 +9,7 @@ jQuery(document).ready(function() {
     jQuery.ajax({
       type: "POST",
       url: "<%=renderResponse.encodeURL(createEmployeeAction.toString())%>",
-      data: {},
+      data: {firstName: 'testname'},
       success: function(msg) {
         console.log(msg);
       },
