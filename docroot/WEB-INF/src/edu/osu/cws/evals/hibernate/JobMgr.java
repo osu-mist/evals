c8fbfe679ff4ef7de629b594ebe23090143f6b51
Alex Ruef <alex.ruef@oregonstate.edu>
2020-06-18 14:39:22 -0700

    try with query

--- docroot/WEB-INF/src/edu/osu/cws/evals/hibernate/JobMgr.java
+++ docroot/WEB-INF/src/edu/osu/cws/evals/hibernate/JobMgr.java
@@ -689,10 +689,12 @@ public class JobMgr {
       job.setSupPidm(supervisor.getId());
       job.setSupPosn(supervisor.getPositionNumber());
       job.setSupSuff(supervisor.getSuffix());
+      String query = "update pyvpasj set PYVPASJ_SUPERVISOR_PIDM = " + supervisor.getId() + ", PYVPASJ_SUPERVISOR_POSN = " + supervisor.getPositionNumber() + ", PYVPASJ_SUPERVISOR_SUFF = " + supervisor.getSuffix() + " where PYVPASJ_PIDM = " + employee.getId();
 
       Session session = HibernateUtil.getCurrentSession();
-      session.save(job);
-      session.flush();
+      session.createSQLQuery(query).list();
+      /*session.save(job);
+      session.flush();*/
 
       return job;
     }
