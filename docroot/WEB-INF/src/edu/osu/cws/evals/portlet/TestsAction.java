496b595d8b5a5d5add56c4c0f4ea78b72074d55a
Alex Ruef <alex.ruef@oregonstate.edu>
2020-07-08 14:58:20 -0700

    setAdminPortletData

--- docroot/WEB-INF/src/edu/osu/cws/evals/portlet/TestsAction.java
+++ docroot/WEB-INF/src/edu/osu/cws/evals/portlet/TestsAction.java
@@ -249,7 +249,7 @@ public class TestsAction implements ActionInterface {
 
       actionHelper.reloadMyAppraisals();
       actionHelper.setupMyTeamActiveAppraisals();
-      actionHelper.refreshContextCache();
+      actionHelper.setAdminPortletData();
       return homeAction.display(request, response);
     }
 
