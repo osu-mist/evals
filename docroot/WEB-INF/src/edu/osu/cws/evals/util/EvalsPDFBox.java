a4af871c3681c11dc1d1f61b8a3e830b163b29ef
Alex Ruef <alex.ruef@oregonstate.edu>
2020-10-14 16:57:38 -0700

    Adding assessment details

--- docroot/WEB-INF/src/edu/osu/cws/evals/util/EvalsPDFBox.java
+++ docroot/WEB-INF/src/edu/osu/cws/evals/util/EvalsPDFBox.java
@@ -34,17 +34,23 @@ public class EvalsPDFBox {
 
     private static PDFont font = PDType1Font.HELVETICA;
     private static PDFont fontBold = PDType1Font.HELVETICA_BOLD;
+    private static PDFont fontBoldItalic = PDType1Font.HELVETICA_BOLD_OBLIQUE;
     private static float fontSize = 10.0f;
     private static float fontSizeBold = 11.0f;
     private static float fontSizeHeaderBold = 14.0f;
 
     private static float sideMargin = 50f;
-    private static float topMargin = 20f;
+    private static float topMargin = 22f;
     private static float lineHeight = 13f;
+    private static float tabSize = 12.5f;
 
     private static final String IMAGE_OSU_LOGO = "images/pdf-osu-logo.png";
     private static final float OSU_LOGO_HEIGHT = 35.875f;
     private static final float OSU_LOGO_WIDTH = 116.625f;
+    private static final String IMAGE_CHECKBOX_CHECKED = "images/pdf-checkbox-checked.jpg";
+    private static final String IMAGE_CHECKBOX_UNCHECKED = "images/pdf-checkbox-unchecked.jpg";
+    private static final float CHECKBOX_HEIGHT = 10f;
+    private static final float CHECKBOX_WIDTH = 10f;
 
     private static float baseRowHeight = 17f;
     private static float cellMargin = 5f;
@@ -54,6 +60,7 @@ public class EvalsPDFBox {
 
     private Appraisal appraisal;
     private List<Rating> ratings;
+    private PermissionRule permRule;
 
     private PDDocument doc;
     private List<PDPage> pages;
@@ -65,21 +72,22 @@ public class EvalsPDFBox {
         this.rootPath = rootPath;
         this.appraisal = appraisal;
         this.ratings = ratings;
+        this.permRule = appraisal.getPermissionRule();
 
         doc = new PDDocument();
         try {
             pages = new ArrayList<PDPage>();
             addPage();
 
-            curLine = getPageHeight() - topMargin;
-
             addHeader();
 
             addInfoTable();
 
-            writeText(font, fontSize, sideMargin, curLine, "test text");
+            addGoalVersions();
 
-            contStream.close();
+            if (contStream != null) {
+                contStream.close();
+            }
             File file = new File("/opt/evals/pdf/" + "testFile.pdf");
             file.createNewFile();
             doc.save(file);
@@ -91,7 +99,7 @@ public class EvalsPDFBox {
     }
 
     private void addHeader() throws IOException {
-        writeImage(rootPath + IMAGE_OSU_LOGO, curLine);
+        writeImage(rootPath + IMAGE_OSU_LOGO, sideMargin, curLine, OSU_LOGO_WIDTH, OSU_LOGO_HEIGHT);
         addToCurLine(OSU_LOGO_HEIGHT);
 
         String officeHr = resource.getString("office-hr");
@@ -114,7 +122,6 @@ public class EvalsPDFBox {
     private void addInfoTable() throws IOException {
         Job job = appraisal.getJob();
         Employee emp = job.getEmployee();
-        PermissionRule permRule = appraisal.getPermissionRule();
 
         String empName = emp.getLastName() + ", " + emp.getFirstName();
         String supName = "";
@@ -126,6 +133,7 @@ public class EvalsPDFBox {
             appraisalTypeKey = "appraisal-type-" + appraisal.getType();
         }
         String ratingText = "";
+        // CHANGE THIS FOR REAL TESTING 
         // boolean displayRating = StringUtils.containsAny(permRule.getEvaluation(), "ev");
         boolean displayRating = true;
         if (appraisal.getRating() != null && displayRating) {
@@ -144,9 +152,97 @@ public class EvalsPDFBox {
         writeTable(curLine, content);
     }
 
-    private void writeImage(String path, float y) throws IOException {
+    private void addGoalVersions() throws IOException {
+        int displayedGoals = 0;
+
+        // CHANGE THIS FOR REAL TESTING 
+        // boolean displayGoals = StringUtils.containsAny(permRule.getApprovedGoals(), "ev");
+        boolean displayGoals = true;
+        if(displayGoals) {
+            List<GoalVersion> approvedGoalsVersions = appraisal.getApprovedGoalsVersions();
+            for (GoalVersion goalVersion : approvedGoalsVersions) {
+                String goalsApproved = resource.getString("appraisal-goals-approved-on") + " " +
+                        new DateTime(goalVersion.getGoalsApprovedDate()).toString(Constants.DATE_FORMAT) + ":";
+                writeText(fontBold, 12f, sideMargin, curLine, goalsApproved, true);
+                addToCurLine(lineHeight * 2);
+
+                List<Assessment> sortedAssessments = goalVersion.getSortedAssessments();
+                displayedGoals = addGoals(displayedGoals, sortedAssessments);
+            }
+        }
+    }
+
+    private int addGoals(int displayedGoals, List<Assessment> sortedAssessments) throws IOException {
+        // CHANGE THIS FOR REAL TESTING 
+        // boolean displayEmployeeResults = StringUtils.containsAny(permRule.getResults(), "ev");
+        // boolean displaySupervisorResults = StringUtils.containsAny(permRule.getSupervisorResults(), "ev");
+        boolean displayEmployeeResults = true;
+        boolean displaySupervisorResults = true;
+
+        for(Assessment assessment : sortedAssessments) {
+            displayedGoals ++;
+
+            // add employee goal
+            String goalLabel = resource.getString("appraisal-goals") + displayedGoals;
+            writeText(fontBoldItalic, fontSize, sideMargin, curLine, goalLabel, true);
+            addToCurLine(lineHeight);
+            writeText(font, fontSize, sideMargin + tabSize, curLine, assessment.getGoal());
+            addToCurLine(lineHeight * 2);
+
+            addAssessmentCriteria(assessment);
+
+            // add employee result
+            String empResultText = resource.getString("appraisal-employee-results");
+            writeText(fontBoldItalic, fontSize, sideMargin + tabSize, curLine, empResultText);
+            addToCurLine(lineHeight);
+            writeText(font, fontSize, sideMargin + tabSize, curLine, assessment.getEmployeeResult());
+            addToCurLine(lineHeight * 2);
+
+            // add supervisor result
+            String supResultText = resource.getString("appraisal-result-comments");
+            writeText(fontBoldItalic, fontSize, sideMargin + tabSize, curLine, supResultText);
+            addToCurLine(lineHeight);
+            writeText(font, fontSize, sideMargin + tabSize, curLine, assessment.getSupervisorResult());
+            addToCurLine(lineHeight * 2);
+        }
+        addToCurLine(lineHeight);
+
+        return displayedGoals;
+    }
+
+    private void addAssessmentCriteria(Assessment assessment) throws IOException {
+        writeText(fontBoldItalic, fontSize, sideMargin + tabSize, curLine, resource.getString("appraisal-selected-criteria"));
+        addToCurLine(lineHeight);
+        float checkboxPadding = 3f;
+        float criteriaPadding = 20f;
+        float initialx = sideMargin + tabSize;
+        float x = initialx;
+
+        for (AssessmentCriteria assessmentCriteria : assessment.getSortedAssessmentCriteria()) {
+            String checkboxPath;
+            if (assessmentCriteria.getChecked() != null && assessmentCriteria.getChecked()) {
+                checkboxPath = rootPath + IMAGE_CHECKBOX_CHECKED;
+            } else {
+                checkboxPath = rootPath + IMAGE_CHECKBOX_UNCHECKED;
+            }
+            String criteriaText = assessmentCriteria.getCriteriaArea().getName().toUpperCase();
+            float criteriaTextWidth = getTextWidth(criteriaText, font, fontSize);
+            if (x + CHECKBOX_WIDTH + checkboxPadding + criteriaTextWidth > getPageWidth() - sideMargin) {
+                x = initialx;
+                addToCurLine(CHECKBOX_HEIGHT + lineHeight);
+            }
+            writeImage(checkboxPath, x, curLine, CHECKBOX_WIDTH, CHECKBOX_HEIGHT);
+            x += CHECKBOX_WIDTH + checkboxPadding;
+            writeText(font, fontSize, x, curLine - CHECKBOX_HEIGHT, criteriaText);
+            x += criteriaTextWidth + criteriaPadding;
+        }
+
+        addToCurLine(CHECKBOX_HEIGHT + lineHeight * 3);
+    }
+
+    private void writeImage(String path, float x, float y, float width, float height) throws IOException {
         PDImageXObject pdImage = PDImageXObject.createFromFile(path, doc);
-        contStream.drawImage(pdImage, sideMargin, y - OSU_LOGO_HEIGHT, OSU_LOGO_WIDTH, OSU_LOGO_HEIGHT);
+        contStream.drawImage(pdImage, x, y - height, width, height);
     }
 
     private void writeTable(float y, String[][][] content) throws IOException {
@@ -208,11 +304,23 @@ public class EvalsPDFBox {
     }
 
     private void writeText(PDFont font, float fontSize, float x, float y, String text) throws IOException {
+        writeText(font, fontSize, x, y, text, false);
+    }
+
+    private void writeText(PDFont font, float fontSize, float x, float y, String text, boolean underline) throws IOException {
         contStream.setFont(font, fontSize);
         contStream.beginText();
         contStream.moveTextPositionByAmount(x, y);
         contStream.drawString(text);
         contStream.endText();
+
+        if(underline) {
+            float textWidth = getTextWidth(text, font, fontSize);
+            float lineHeight = y - 2f;
+
+            contStream.setLineWidth(1f);
+            contStream.drawLine(x, lineHeight, textWidth + x, lineHeight);
+        }
     }
 
     private float findRowHeight(float baseRowHeight, float baseColWidth, String[][] content) throws IOException {
@@ -253,14 +361,18 @@ public class EvalsPDFBox {
         page.setMediaBox(PDRectangle.LETTER);
         doc.addPage(page);
         pages.add(page);
+        if (contStream != null) {
+            contStream.close();
+        }
         contStream = new PDPageContentStream(doc, page);
-        curLine = topMargin;
+        curLine = getPageHeight() - topMargin;
     }
 
     private void addToCurLine(float height) throws IOException {
-        curLine -= height;
-        if (curLine <= 0) {
+        if (curLine - height <= topMargin) {
             addPage();
+        } else {
+            curLine -= height;
         }
     }
 }
