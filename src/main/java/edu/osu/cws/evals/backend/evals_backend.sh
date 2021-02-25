17bec0d181c4d7df645f8c92593db7cb43fb7355
Alex Ruef <alex.ruef@oregonstate.edu>
2021-02-24 16:19:13 -0800

    updated classpath with new class versions

--- src/main/java/edu/osu/cws/evals/backend/evals_backend.sh
+++ src/main/java/edu/osu/cws/evals/backend/evals_backend.sh
@@ -31,18 +31,19 @@ CLASSPATH=$CLASSPATH:$LIB_DIR/commons-collections-3.1.jar
 CLASSPATH=$CLASSPATH:$LIB_DIR/commons-configuration-1.6.jar
 CLASSPATH=$CLASSPATH:$LIB_DIR/commons-email-1.3.1.jar
 CLASSPATH=$CLASSPATH:$LIB_DIR/commons-lang-2.6.jar
-CLASSPATH=$CLASSPATH:$LIB_DIR/commons-logging-1.1.1.jar
+CLASSPATH=$CLASSPATH:$LIB_DIR/commons-logging-1.2.jar
 CLASSPATH=$CLASSPATH:$LIB_DIR/dbunit-2.4.8.jar
 CLASSPATH=$CLASSPATH:$LIB_DIR/dom4j-1.6.1.jar
-CLASSPATH=$CLASSPATH:$LIB_DIR/gelfj-1.0.2.jar
+CLASSPATH=$CLASSPATH:$LIB_DIR/org.graylog2.gelfj-1.0.2.jar
 CLASSPATH=$CLASSPATH:$LIB_DIR/guice-3.0.jar
-CLASSPATH=$CLASSPATH:$LIB_DIR/hibernate3.jar
+CLASSPATH=$CLASSPATH:$LIB_DIR/hibernate-core-3.6.3.Final.jar
 CLASSPATH=$CLASSPATH:$LIB_DIR/hibernate-jpa-2.0-api-1.0.0.Final.jar
+CLASSPATH=$CLASSPATH:$LIB_DIR/hibernate-commons-annotations-3.2.0.Final.jar
 CLASSPATH=$CLASSPATH:$LIB_DIR/javax.inject.jar
 CLASSPATH=$CLASSPATH:$LIB_DIR/joda-time-2.2.jar
 CLASSPATH=$CLASSPATH:$LIB_DIR/ojdbc8.jar
 CLASSPATH=$CLASSPATH:$LIB_DIR/esources_en.jar
-CLASSPATH=$CLASSPATH:$LIB_DIR/slf4j-api-1.6.1.jar
+CLASSPATH=$CLASSPATH:$LIB_DIR/slf4j-api-1.7.26.jar
 CLASSPATH=$CLASSPATH:$LIB_DIR/slf4j-jdk14-1.6.1.jar
 CLASSPATH=$CLASSPATH:$LIB_DIR/util-bridges.jar
 CLASSPATH=$CLASSPATH:$LIB_DIR/util-java.jar
@@ -51,11 +52,11 @@ CLASSPATH=$CLASSPATH:$LIB_DIR/javassist-3.12.0.GA.jar
 CLASSPATH=$CLASSPATH:$LIB_DIR/jstl-impl.jar
 CLASSPATH=$CLASSPATH:$LIB_DIR/jstl.jar
 CLASSPATH=$CLASSPATH:$LIB_DIR/resources_en.jar
-CLASSPATH=$CLASSPATH:$LIB_DIR/javax.mail-1.4.4.jar
+CLASSPATH=$CLASSPATH:$LIB_DIR/javax.mail-1.6.2.jar
 CLASSPATH=$CLASSPATH:/$LIB_DIR/opencsv-2.3.jar
 CLASSPATH=$CLASSPATH:/$PORTAL_LIB_EXT/portal-service.jar
+CLASSPATH=$CLASSPATH:/$PORTAL_LIB_EXT/portal-kernel.jar
 CLASSPATH=$CLASSPATH:/$PORTAL_LIB_EXT/jta.jar
 
 cd $PORTLET_ROOT
 $JAVA_HOME/bin/java -classpath $CLASSPATH edu.osu.cws.evals.backend.BackendAction
-
